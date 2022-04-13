d3f0dd7ed0c7dddbf47b3c101f293a924998dea1
Amar Parnate <amar.parnate@microlise.com>
2022-04-13 21:05:37 +0530

    add total amount

--- views/CartItems.php
+++ views/CartItems.php
@@ -16,7 +16,10 @@ $cartProducts = $productCart->getCartProducts();
                 <th>Action</th>
             </tr>
             <?php
-            foreach ($cartProducts as $item) { ?>
+            $totalPrice = 0;
+            foreach ($cartProducts as $item) {
+                $totalPrice += $item['price'];
+                ?>
                 <tr>
                     <td><?php echo $item['product_name'] ?></td>
                     <td><?php echo $item['sku'] ?></td>
@@ -26,19 +29,29 @@ $cartProducts = $productCart->getCartProducts();
                         <?php echo $item['price'] ?>
                     </td>
                     <td>
-                        <form id="remove_<?php echo $item['product_id'] ?>" action="includes/AddToCartProcess.php" method="post">
+                        <form id="remove_<?php echo $item['product_id'] ?>" action="includes/AddToCartProcess.php"
+                              method="post">
                             <input type="hidden" name="action" value="remove_from_cart">
-                            <input type="hidden" name="product_id" value="<?php echo $item['product_id'] ?>" />
+                            <input type="hidden" name="product_id" value="<?php echo $item['product_id'] ?>"/>
                             <button class="btn">Remove</button>
                         </form>
                     </td>
                 </tr>
             <?php } ?>
+            <tr>
+                <td colspan="5" class="text-right pr-10">
+                  <h2>  <b>Total:
+                        <?php echo $item['currency'] ?>
+                        <?php echo $totalPrice; ?>
+                    </b>
+                  </h2>
+                </td>
+            </tr>
             </tbody>
         </table>
     </div>
 <?php } else { ?>
     <div>
-        <span class="text-red">The cart is empty.</span>
+        <span class="text-red ">The cart is empty.</span>
     </div>
 <?php } ?>
